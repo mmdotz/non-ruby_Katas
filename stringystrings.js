5c1508dde0258fbf1d4dab19fcaf3492325e622f
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-22 09:24:52 -0500

    first commit

---
+++ stringystrings.js
@@ -0,0 +1,24 @@
+// Description:
+//
+// write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+//
+// the string should start with a 1.
+//
+// a string with size 6 should return :'101010'.
+//
+// with size 4 should return : '1010'.
+//
+// with size 12 should return : '101010101010'.
+//
+// The size will always be positive and will only use whole numbers.
+
+// getting started, need to eval odd or even
+function stringy(size) {
+  if (size > 1) {
+    console.log(1 * size);
+  } else {
+    console.log(1);
+  }
+}
+
+stringy(3);
